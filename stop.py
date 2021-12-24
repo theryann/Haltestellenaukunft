2d9bfd0d2cc8eece756b616370cad36c62f49105
theryann <darianpatzak@web.de>
2021-12-24 05:14:40 +0100

    alle 30 sekunden fenster neu gestartet mit aktuellen daten. is halt  unsauber

--- stop.py
+++ stop.py
@@ -5,17 +5,20 @@ class Haltestelle:
 
     def __init__(self, haltestelle='Alberplatz'):
         self.stop = haltestelle
+        self.departures = dvb.monitor(self.stop, 0, 10, 'Dresden')
+
+    def on_after(self):
+
+        self.departures = dvb.monitor(self.stop, 0, 10, 'Dresden')
+        self.show()
+
 
     def show(self):
-        time_offset = 0
-        num_results = 10
-        city = 'Dresden'
-
-        departures = dvb.monitor(self.stop, time_offset, num_results, city)
+        
 
         # zur abschätzung wie weit die namen eingerückt sein müssen
         max_line_width = 0
-        for dep in departures:
+        for dep in self.departures:
             if len(dep['line']) > max_line_width:
                 max_line_width = len(dep['line'])
 
@@ -24,16 +27,16 @@ class Haltestelle:
         bg_color ="#101010"     #Globale Hintergrundfarbe
 
         root = Tk()
-        root.title("Rubik's Cube")
+        root.title("Abfahrten")
         root.geometry("500x507+600+200")
         root.configure(background=bg_color)
 
         # Attriblutes
-        font_face = "Arial"
+        font_face = "Courier New"
         line_text_size = 20
         top_margin = 60
         margin_line = 10
-        margin_direction = margin_line + max_line_width * line_text_size / 2.0  + 20
+        margin_direction = margin_line + (max_line_width * line_text_size / 1.5)  + 30
         margin_min = 400
 
         # Pivot Haltestelle
@@ -41,9 +44,11 @@ class Haltestelle:
         haltestelle_text.pack()
 
         # Abfahrten
-        for line in departures:
+        for line in self.departures:
 
-            line_label = Label(text = line['line'], fg = "Yellow", bg=bg_color, font=(font_face, line_text_size))
+            line_text = ' ' * (max_line_width - len(line['line'])) + line['line']
+
+            line_label = Label(text = line_text, fg = "Yellow", bg=bg_color, font=(font_face, line_text_size))
             line_label.pack()
             line_label.place(x=margin_line, y=top_margin)
 
@@ -66,10 +71,12 @@ class Haltestelle:
             top_margin += line_text_size * 2
 
         # Mainloop
+        root.after(30000, root.destroy)
         root.mainloop()
+        self.on_after()
 
 
 hs2 = Haltestelle('Albertplatz')
 hs2.show()
-hs = Haltestelle('Tronitzer Straße')
-hs.show()
+# hs = Haltestelle('Tronitzer Straße')
+# hs.show()
